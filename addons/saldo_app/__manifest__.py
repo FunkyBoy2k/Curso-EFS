031caa75f307c73c79fb61c7ed7a4fb947d8bdfa
Robinson GeSe <65371987+FunkyBoy2k@users.noreply.github.com>
2023-10-10 17:40:00 -0500

    on_tread correcto

--- addons/saldo_app/__manifest__.py
+++ addons/saldo_app/__manifest__.py
@@ -3,7 +3,8 @@
     'description':"Aplicaicon web para registrar ingresos y egresos",
     'author':"Robinson Gutierrez",
     'version':"1.1",
-    'depends':["base"],
+    #se importa mail para que pueda ocupar el atributo
+    'depends':["base","mail"],
     'data':[
         "views/views.xml",
         "security/ir_model_access.xml"
--- addons/saldo_app/models/models.py
+++ addons/saldo_app/models/models.py
@@ -3,17 +3,18 @@ from odoo import fields, models
 class Movimiento(models.Model):
     _name = "sa.movimiento" 
     _description = "Movimineto"
+    _inherit = "mail.thread"
 
     name = fields.Char(string ="Nombre",required=True)
     type_move = fields.Selection(selection=[("ingreso","Ingreso"),("gasto","Gasto")],string ="Tipo",default="ingreso",required=True)
     date = fields.Datetime(string ="Fecha")
-    amount =  fields.Float("Monto")
+    amount =  fields.Float("Monto",track_visibility="onchange")
     recipt_image = fields.Binary("Foto del recibo")
     notas = fields.Html("Notas")
     currency_id = fields.Many2one("res.currency",default=162)
     user_id = fields.Many2one("res.users",string="Usuario")
     category_id = fields.Many2one("sa.category","Categoria")
-
+    
     tag_ids = fields.Many2many("sa.tag","sa_mov_sa_tag_rel","move_id","tag_id")
 
 
